2d5ba484ffd90870bda5139aad7906bc5b5ff226
Romulo Zoch <romulozoch@inkmotion.com.br>
2013-10-08 20:09:03 -0300

    Now facebook login defines the name (based on ID) of the table we'll be working on.

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 var app = angular.module("atendapp", ["firebase"]);
       function MyController($scope, angularFire, angularFireAuth) {
 //Login e Logout
-      	$scope.login = function() {
-  angularFireAuth.login("facebook");
-};
-$scope.logout = function() {
-  angularFireAuth.logout();
-};
-
+		$scope.login = function() { angularFireAuth.login("facebook", {rememberMe: true,scope: 'email,user_likes'});};
+		$scope.logout = function() { angularFireAuth.logout();};
+		$scope.$on("angularFireAuth:login", function(evt, user) {
+               $scope.meuid =user.id;
+               var ref2 = new Firebase("https://alfapixteste.firebaseio.com/app/users/" + user.id);
+               
+});
+        
         var ref = new Firebase("https://alfapixteste.firebaseio.com/");
-        $scope.messages = [];
+        var auth = new FirebaseSimpleLogin(ref, function(error, user) {
+       if(user){
         angularFireAuth.initialize(ref, {scope: $scope, name: "user"});
-        angularFire(ref, $scope, "messages");
+        $scope.messages = [];
+        var trueref = ref.child("users").child(user.id);
+        angularFire(trueref, $scope, "messages");
         $scope.addMessage = function(e) {
           if (e.keyCode != 13) return;
-          $scope.messages.push({idusr: $scope.user.id, from: $scope.user.name, body: $scope.msg});
+          alert($scope.meuid)
+          $scope.messages.push({idusr: $scope.user.id, from: $scope.user.first_name, body: $scope.msg});
           $scope.msg = "";
         };
+       }
+        });
       }
